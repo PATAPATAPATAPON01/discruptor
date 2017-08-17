46f7347ee08860a334d41c122d909c684972b717
za-lvjian <lvjian@zhongan.io>
2017-08-17 18:01:46 +0800

    Initial commit

---
+++ src/main/java/quickstart/LongEventFactory.java
@@ -0,0 +1,11 @@
+package quickstart;
+
+import com.lmax.disruptor.EventFactory;
+
+public class LongEventFactory implements EventFactory<LongEvent>
+{
+    public LongEvent newInstance()
+    {
+        return new LongEvent();
+    }
+}
---
+++ src/main/java/quickstart/LongEventHandler.java
@@ -0,0 +1,12 @@
+package quickstart;
+
+import com.lmax.disruptor.EventHandler;
+
+public class LongEventHandler implements EventHandler<LongEvent>
+{
+    public void onEvent(LongEvent event, long sequence, boolean endOfBatch)
+    {
+
+        System.out.println("Event: " + event.getValue());
+    }
+}
